2372b945ba0cb01be62fa552cdcfe33f9a30bff7
Hagen Morano <mail@hagenmorano.de>
2019-01-07 17:48:00 +0100

    gitignore everything but dist

--- dist/esm5/public_api.js
+++
@@ -1,6 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-export { RadialProgressModule } from './src/app/modules/radial-progress/radial-progress.module';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljX2FwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL25neC1yYWRpYWwtcHJvZ3Jlc3MvIiwic291cmNlcyI6WyJwdWJsaWNfYXBpLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7QUFBQSxxQ0FBYywwREFBMEQsQ0FBQSIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCAqIGZyb20gJy4vc3JjL2FwcC9tb2R1bGVzL3JhZGlhbC1wcm9ncmVzcy9yYWRpYWwtcHJvZ3Jlc3MubW9kdWxlJ1xuIl19
